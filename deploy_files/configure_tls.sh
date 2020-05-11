53384fd3bf7ba21a60285ff6a2fbad1bfb5d0c85
wintermaples <pg.wintermaples@gmail.com>
2020-05-11 18:58:39 +0900

    Fix configure_tls

--- deploy_files/configure_tls.sh
+++ deploy_files/configure_tls.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo certbot --nginx -w /var/www/html/ -d www.suguvote.net -d suguvote.net -m tpgwinter@gmail.com --agree-tos -n
-sudo certbot certonly --nginx -w /var/www/html/ -d api.suguvote.net -m tpgwinter@gmail.com --agree-tos -n
+sudo certbot --webroot certonly -w /var/www/html/ -d api.suguvote.net -m tpgwinter@gmail.com --agree-tos -n
