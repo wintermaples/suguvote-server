5a253d3a77bac68a361801a42d6a06c1756a8930
wintermaples <pg.wintermaples@gmail.com>
2020-05-11 19:11:19 +0900

    Fix deploy_latest

--- inserver/deploy_latest.sh
+++ inserver/deploy_latest.sh
@@ -2,13 +2,13 @@
 
 sudo systemctl stop nginx
 sudo systemctl stop emperor.uwsgi.service
-sudo systemctl stop mongob
+sudo systemctl stop mongod
 
 sudo rm -R suguvote-server/
 git clone git@suguvote-server:wintermaples/suguvote-server.git suguvote-server
 chmod +x suguvote-server/deploy.sh
 ./suguvote-server/deploy.sh
 
-sudo systemctl restart mongob
+sudo systemctl restart mongod
 sudo systemctl restart emperor.uwsgi.service
 sudo systemctl restart nginx
