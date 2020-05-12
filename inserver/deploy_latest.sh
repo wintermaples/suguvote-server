470a53c43369744315b8805cd00810a423837f68
wintermaples <pg.wintermaples@gmail.com>
2020-05-12 20:49:37 +0900

    Set branch to alpha-dev1

--- inserver/deploy_latest.sh
+++ inserver/deploy_latest.sh
@@ -5,7 +5,7 @@ sudo systemctl stop emperor.uwsgi.service
 sudo systemctl stop mongod
 
 sudo rm -R suguvote-server/
-git clone git@suguvote-server:wintermaples/suguvote-server.git suguvote-server
+git clone -b alpha-dev1 git@suguvote-server:wintermaples/suguvote-server.git suguvote-server
 chmod +x suguvote-server/deploy.sh
 ./suguvote-server/deploy.sh
 
