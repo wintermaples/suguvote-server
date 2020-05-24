862a1eaa03d2b40c89223fd4d2c64328d1fed281
wintermaples <pg.wintermaples@gmail.com>
2020-05-25 00:10:32 +0900

    Test.

--- deploy_files/configure_tls.sh
+++ deploy_files/configure_tls.sh
@@ -4,6 +4,6 @@ if [ $NO_USE_TLS != "True" ]; then
   sudo certbot --nginx -w /var/www/html/ -d www.suguvote.net -d suguvote.net -m tpgwinter@gmail.com --agree-tos -n
   sudo certbot --webroot certonly -w /var/www/html/ -d api.suguvote.net -m tpgwinter@gmail.com --agree-tos -n
 else
-  sudo certbot --staging -w /var/www/html/ -d www.suguvote.net -d suguvote.net -m tpgwinter@gmail.com --agree-tos -n
-  sudo certbot --staging certonly -w /var/www/html/ -d api.suguvote.net -m tpgwinter@gmail.com --agree-tos -n
+  sudo certbot --staging -d www.suguvote.net -d suguvote.net -m tpgwinter@gmail.com --agree-tos -n
+  sudo certbot --staging -d api.suguvote.net -m tpgwinter@gmail.com --agree-tos -n
 fi
