baf0e3a201e3ec3585feac6b703b4a7e68107bb0
wintermaples <pg.wintermaples@gmail.com>
2020-06-04 00:41:30 +0900

    Fix

--- deploy_files/configure_tls.sh
+++ deploy_files/configure_tls.sh
@@ -3,8 +3,8 @@
 if [ "$FORCE_RENEWAL_TLS" = "True" ]; then
   echo "===== FORCE_RENEWAL_TLS is True ====="
   sudo certbot --force-renewal --nginx -w /var/www/html/ -d www.suguvote.net -d suguvote.net -m tpgwinter@gmail.com --agree-tos -n
-  sudo certbot --force-renewal --certonly --webroot -w /var/www/html/ -d api.suguvote.net -m tpgwinter@gmail.com --agree-tos -n
+  sudo certbot --force-renewal certonly --webroot -w /var/www/html/ -d api.suguvote.net -m tpgwinter@gmail.com --agree-tos -n
 else
   sudo certbot --nginx -w /var/www/html/ -d www.suguvote.net -d suguvote.net -m tpgwinter@gmail.com --agree-tos -n
-  sudo certbot --webroot --certonly -w /var/www/html/ -d api.suguvote.net -m tpgwinter@gmail.com --agree-tos -n
+  sudo certbot --webroot certonly -w /var/www/html/ -d api.suguvote.net -m tpgwinter@gmail.com --agree-tos -n
 fi
